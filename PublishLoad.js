ba4074f5d203371e27e1d9b9fdb0a34238890764
mProjectsCode <m.projects.code@gmail.com>
2023-06-14 16:09:30 +0200

    add git attributes and publish load script

---
+++ PublishLoad.js
@@ -0,0 +1,30 @@
+// META-BIND OBSIDIAN PUBLISH SCRIPT
+// add this script to the end of you publish.js
+// this script loads and executes the latest version of the meta-bind plugin for obsidian publish
+
+// The plugin settings. Not all settings are used.
+let mb_settingsString = `{
+	"devMode": false,
+	"preferredDateFormat": "YYYY-MM-DD",
+	"useUsDateInputOrder": false,
+	"syncInterval": 50,
+	"minSyncInterval": 1000,
+	"maxSyncInterval": 50,
+	"inputTemplates": "",
+	"dateFormat": "eu"
+}`;
+
+// if true, the script is pulled from the master branch instead of the release branch
+const mb_dev_mode = false;
+
+const mb_settings = JSON.parse(mb_settingsString);
+
+async function mb_load() {
+	const response = mb_dev_mode
+		? await fetch('https://raw.githubusercontent.com/mProjectsCode/obsidian-meta-bind-plugin/master/Publish.js')
+		: await fetch('https://raw.githubusercontent.com/mProjectsCode/obsidian-meta-bind-plugin/release/Publish.js');
+	const script = await response.text();
+	eval(script);
+}
+
+await mb_load();
