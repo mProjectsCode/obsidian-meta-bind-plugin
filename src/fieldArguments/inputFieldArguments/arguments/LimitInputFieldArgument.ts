dfd110641ec157364596e27beca679b56e3fa74f
Daniel Barenholz <d.barenholz@student.tue.nl>
2023-10-25 13:16:30 +0200

    Add 'Limit' to Text and TextArea fields

---
+++ src/fieldArguments/inputFieldArguments/arguments/LimitInputFieldArgument.ts
@@ -0,0 +1,31 @@
+import { AbstractInputFieldArgument } from '../AbstractInputFieldArgument';
+import { type InputFieldArgumentConfig, InputFieldArgumentConfigs } from '../../../parsers/inputFieldParser/InputFieldConfigs';
+import { type ParsingResultNode } from '../../../parsers/nomParsers/GeneralParsers';
+import { ErrorLevel, MetaBindParsingError } from '../../../utils/errors/MetaBindErrors';
+
+export class LimitInputFieldArgument extends AbstractInputFieldArgument {
+	value: number | undefined = undefined;
+
+	_parseValue(value: ParsingResultNode[]): void {
+		this.value = Number.parseInt(value[0].value);
+		if (Number.isNaN(this.value)) {
+			throw new MetaBindParsingError(
+				ErrorLevel.WARNING,
+				'failed to set value for input field argument',
+				"value of argument 'limit' must be of type number",
+			);
+		}
+
+		if (this.value <= 0) {
+			throw new MetaBindParsingError(
+				ErrorLevel.WARNING,
+				'failed to set value for input field argument',
+				"value of argument 'limit' must be positive",
+			);
+		}
+	}
+
+	public getConfig(): InputFieldArgumentConfig {
+		return InputFieldArgumentConfigs.limit;
+	}
+}
--- src/inputFields/_new/fields/TextArea/TextAreaComponent.svelte
+++ src/inputFields/_new/fields/TextArea/TextAreaComponent.svelte
@@ -1,6 +1,8 @@
 <script lang='ts'>
 	export let value: string;
 	export let placeholder: string;
+	export let limit: number | undefined;
+
 	export let onValueChange: (value: string) => void;
 
 	export function setValue(v: string): void {
@@ -8,4 +10,5 @@
 	}
 </script>
 
-<textarea tabindex='0' placeholder={placeholder} bind:value={value} on:input={() => onValueChange(value)}></textarea>
+<textarea tabindex='0' placeholder={placeholder} bind:value={value} maxlength={limit}
+		  on:input={() => onValueChange(value)}></textarea>
