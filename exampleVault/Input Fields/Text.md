c3d4be56fcd970699bcce1b4ed9b7fac1fd1d250
mProjectsCode <m.projects.code@gmail.com>
2023-10-27 22:15:16 +0200

    implement #130

--- exampleVault/Input Fields/Text.md
+++ exampleVault/Input Fields/Text.md
@@ -1,11 +1,15 @@
 ---
-text: test
+text: alnsdksdnfknfnjksdbfhjdf
 textArea: textArea
 ---
 
 ### Text
 ```meta-bind
 INPUT[text(showcase):text]
+```
+
+```meta-bind
+INPUT[text(showcase, limit(10)):text]
 ```
 
 ### Text Area
--- src/inputFields/_new/fields/Text/TextComponent.svelte
+++ src/inputFields/_new/fields/Text/TextComponent.svelte
@@ -7,7 +7,16 @@
 	export function setValue(v: string): void {
 		value = v;
 	}
+
+	function getLimitString(length: number, limit: number) {
+		const limitStr = limit.toString();
+		const lengthStr = length.toString().padStart(limitStr.length, '0');
+		return `${lengthStr}/${limitStr}`;
+	}
 </script>
 
 <input type='text' tabindex='0' placeholder={placeholder} bind:value={value} maxlength={limit}
 	   on:input={() => onValueChange(value)}>
+{#if limit !== undefined}
+	<span class={`mb-content-limit-indicator ${value.length > limit ? 'mb-content-limit-indicator-overflow' : ''}`} >{getLimitString(value.length, limit)}</span>
+{/if}
