15042246a1dd182e431920b106e20160260693d3
Moritz Jung <m.projects.code@gmail.com>
2024-01-29 12:27:57 +0100

    fix release script

--- automation/shellUtils.ts
+++ automation/shellUtils.ts
@@ -21,7 +21,7 @@ export async function $(
 
 	const proc = exec(cmd);
 	const stdout = await new Response(proc.stdout).text();
-	const stderr = await new Response(proc.stdout).text();
+	const stderr = await new Response(proc.stderr).text();
 
 	if (verboseness === Verboseness.VERBOSE) {
 		if (stdout !== '') {
