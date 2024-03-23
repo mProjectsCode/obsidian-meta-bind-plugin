da5c1ca1a180a29a97e19c862c6c434396ff3cfd
Moritz Jung <m.projects.code@gmail.com>
2024-03-23 17:48:51 +0100

    remove time parser

--- packages/core/src/parsers/TimeParser.ts
+++ packages/core/src/parsers/TimeParser.ts
@@ -1,114 +1,116 @@
-import { P_UTILS } from '@lemons_dev/parsinom/lib/ParserUtils';
-import { P } from '@lemons_dev/parsinom/lib/ParsiNOM';
+// import { P_UTILS } from '@lemons_dev/parsinom/lib/ParserUtils';
+// import { P } from '@lemons_dev/parsinom/lib/ParsiNOM';
+//
+// export class Time {
+// 	private _hour: number;
+// 	private _minute: number;
+//
+// 	constructor() {
+// 		this._hour = TimeParser.getDefaultHour();
+// 		this._minute = TimeParser.getDefaultHour();
+// 	}
+//
+// 	public getHour(): number {
+// 		return this._hour;
+// 	}
+//
+// 	public setHour(value: number): void {
+// 		if (value < 0 || value > 24) {
+// 			this._hour = TimeParser.getDefaultHour();
+// 			return;
+// 		}
+// 		this._hour = value;
+// 	}
+//
+// 	public getMinute(): number {
+// 		return this._minute;
+// 	}
+//
+// 	public setMinute(value: number): void {
+// 		if (value < 0 || value > 59) {
+// 			this._minute = TimeParser.getDefaultMinute();
+// 			return;
+// 		}
+// 		this._minute = value;
+// 	}
+//
+// 	public getUniformHour(): string {
+// 		return this.getHour().toString().padStart(2, '0');
+// 	}
+//
+// 	public getUniformMinute(): string {
+// 		return this.getMinute().toString().padStart(2, '0');
+// 	}
+//
+// 	public setHourFromString(str: string): void {
+// 		const v = Number.parseInt(str);
+// 		this.setHour(Number.isNaN(v) ? TimeParser.getDefaultHour() : v);
+// 	}
+//
+// 	public setMinuteFromString(str: string): void {
+// 		const v = Number.parseInt(str);
+// 		this.setMinute(Number.isNaN(v) ? TimeParser.getDefaultMinute() : v);
+// 	}
+// }
+//
+// const timeParser = P.sequenceMap(
+// 	(a, b, _, c, d) => {
+// 		return {
+// 			hour: Number.parseInt(a + b),
+// 			minute: Number.parseInt(c + d),
+// 		};
+// 	},
+// 	P_UTILS.digit(),
+// 	P_UTILS.digit(),
+// 	P.string(':'),
+// 	P_UTILS.digit(),
+// 	P_UTILS.digit(),
+// );
+//
+// export class TimeParser {
+// 	public static parse(timeString: string | null | undefined): Time | undefined {
+// 		if (!timeString) {
+// 			return undefined;
+// 		}
+//
+// 		const time: Time = TimeParser.getDefaultTime();
+//
+// 		const parsedTime = timeParser.tryParse(timeString);
+//
+// 		if (!parsedTime.success) {
+// 			return undefined;
+// 		}
+// 		if (Number.isNaN(parsedTime.value.hour) || Number.isNaN(parsedTime.value.minute)) {
+// 			return undefined;
+// 		}
+// 		if (parsedTime.value.hour < 0 || parsedTime.value.hour >= 24) {
+// 			return undefined;
+// 		}
+// 		if (parsedTime.value.minute < 0 || parsedTime.value.minute >= 60) {
+// 			return undefined;
+// 		}
+//
+// 		time.setHour(parsedTime.value.hour);
+// 		time.setMinute(parsedTime.value.minute);
+//
+// 		return time;
+// 	}
+//
+// 	public static stringify(time: Time): string {
+// 		return `${time.getUniformHour()}:${time.getUniformMinute()}`;
+// 	}
+//
+// 	public static getDefaultTime(): Time {
+// 		return new Time();
+// 	}
+//
+// 	public static getDefaultHour(): number {
+// 		return 0;
+// 	}
+//
+// 	public static getDefaultMinute(): number {
+// 		return 0;
+// 	}
+// }
 
-export class Time {
-	private _hour: number;
-	private _minute: number;
-
-	constructor() {
-		this._hour = TimeParser.getDefaultHour();
-		this._minute = TimeParser.getDefaultHour();
-	}
-
-	public getHour(): number {
-		return this._hour;
-	}
-
-	public setHour(value: number): void {
-		if (value < 0 || value > 24) {
-			this._hour = TimeParser.getDefaultHour();
-			return;
-		}
-		this._hour = value;
-	}
-
-	public getMinute(): number {
-		return this._minute;
-	}
-
-	public setMinute(value: number): void {
-		if (value < 0 || value > 59) {
-			this._minute = TimeParser.getDefaultMinute();
-			return;
-		}
-		this._minute = value;
-	}
-
-	public getUniformHour(): string {
-		return this.getHour().toString().padStart(2, '0');
-	}
-
-	public getUniformMinute(): string {
-		return this.getMinute().toString().padStart(2, '0');
-	}
-
-	public setHourFromString(str: string): void {
-		const v = Number.parseInt(str);
-		this.setHour(Number.isNaN(v) ? TimeParser.getDefaultHour() : v);
-	}
-
-	public setMinuteFromString(str: string): void {
-		const v = Number.parseInt(str);
-		this.setMinute(Number.isNaN(v) ? TimeParser.getDefaultMinute() : v);
-	}
-}
-
-const timeParser = P.sequenceMap(
-	(a, b, _, c, d) => {
-		return {
-			hour: Number.parseInt(a + b),
-			minute: Number.parseInt(c + d),
-		};
-	},
-	P_UTILS.digit(),
-	P_UTILS.digit(),
-	P.string(':'),
-	P_UTILS.digit(),
-	P_UTILS.digit(),
-);
-
-export class TimeParser {
-	public static parse(timeString: string | null | undefined): Time | undefined {
-		if (!timeString) {
-			return undefined;
-		}
-
-		const time: Time = TimeParser.getDefaultTime();
-
-		const parsedTime = timeParser.tryParse(timeString);
-
-		if (!parsedTime.success) {
-			return undefined;
-		}
-		if (Number.isNaN(parsedTime.value.hour) || Number.isNaN(parsedTime.value.minute)) {
-			return undefined;
-		}
-		if (parsedTime.value.hour < 0 || parsedTime.value.hour >= 24) {
-			return undefined;
-		}
-		if (parsedTime.value.minute < 0 || parsedTime.value.minute >= 60) {
-			return undefined;
-		}
-
-		time.setHour(parsedTime.value.hour);
-		time.setMinute(parsedTime.value.minute);
-
-		return time;
-	}
-
-	public static stringify(time: Time): string {
-		return `${time.getUniformHour()}:${time.getUniformMinute()}`;
-	}
-
-	public static getDefaultTime(): Time {
-		return new Time();
-	}
-
-	public static getDefaultHour(): number {
-		return 0;
-	}
-
-	public static getDefaultMinute(): number {
-		return 0;
-	}
-}
+export {};
--- tests/parserTests/TimeParser.test.ts
+++ tests/parserTests/TimeParser.test.ts
@@ -1,85 +1,87 @@
-import { beforeEach, describe, expect, test } from 'bun:test';
-import { Time, TimeParser } from '../../packages/core/src/parsers/TimeParser';
+// import { beforeEach, describe, expect, test } from 'bun:test';
+// import { Time, TimeParser } from '../../packages/core/src/parsers/TimeParser';
+//
+// describe('time stringify', () => {
+// 	describe('should stringify time', () => {
+// 		test.each([
+// 			['00:00', 0, 0],
+// 			['01:05', 1, 5],
+// 			['03:25', 3, 25],
+// 			['23:59', 23, 59],
+// 		])('%s', (expected, hour, minute) => {
+// 			const time = new Time();
+// 			time.setHour(hour);
+// 			time.setMinute(minute);
+//
+// 			expect(TimeParser.stringify(time)).toEqual(expected);
+// 		});
+// 	});
+// });
+//
+// describe('time parser', () => {
+// 	describe('should succeed on valid time', () => {
+// 		test.each([
+// 			['00:00', 0, 0],
+// 			['01:05', 1, 5],
+// 			['03:25', 3, 25],
+// 			['23:59', 23, 59],
+// 		])('%s', (time, expectedHour, expectedMinute) => {
+// 			expect(TimeParser.parse(time)?.getHour()).toBe(expectedHour);
+// 			expect(TimeParser.parse(time)?.getMinute()).toBe(expectedMinute);
+// 		});
+// 	});
+//
+// 	describe('should fail on invalid time format', () => {
+// 		test.each([['00:0'], ['0:05'], ['304:25'], ['25a'], [''], ['23h:15m'], ['23#15']])('%s', time => {
+// 			expect<Time | undefined>(TimeParser.parse(time)).toBe(undefined);
+// 		});
+// 	});
+//
+// 	describe('should fail on time out of bounds', () => {
+// 		test.each([['-01:00'], ['24:05'], ['26:05'], ['05:60'], ['05:99']])('%s', time => {
+// 			expect<Time | undefined>(TimeParser.parse(time)).toBe(undefined);
+// 		});
+// 	});
+// });
+//
+// describe('time class', () => {
+// 	describe('should not be possible to crete invalid time', () => {
+// 		let time: Time;
+//
+// 		beforeEach(() => {
+// 			time = new Time();
+// 		});
+//
+// 		test('set minute to 100', () => {
+// 			time.setMinute(100);
+// 			expect(time.getMinute()).not.toEqual(100);
+// 		});
+//
+// 		test('set minute to -1', () => {
+// 			time.setMinute(-1);
+// 			expect(time.getMinute()).not.toEqual(-1);
+// 		});
+//
+// 		test("set minute to 'a'", () => {
+// 			time.setMinuteFromString('a');
+// 			expect(time.getMinute()).not.toEqual('a');
+// 		});
+//
+// 		test('set hour to 50', () => {
+// 			time.setHour(50);
+// 			expect(time.getHour()).not.toEqual(50);
+// 		});
+//
+// 		test('set hour to -1', () => {
+// 			time.setHour(-1);
+// 			expect(time.getHour()).not.toEqual(-1);
+// 		});
+//
+// 		test("set hour to 'a'", () => {
+// 			time.setHourFromString('a');
+// 			expect(time.getHour()).not.toEqual('a');
+// 		});
+// 	});
+// });
 
-describe('time stringify', () => {
-	describe('should stringify time', () => {
-		test.each([
-			['00:00', 0, 0],
-			['01:05', 1, 5],
-			['03:25', 3, 25],
-			['23:59', 23, 59],
-		])('%s', (expected, hour, minute) => {
-			const time = new Time();
-			time.setHour(hour);
-			time.setMinute(minute);
-
-			expect(TimeParser.stringify(time)).toEqual(expected);
-		});
-	});
-});
-
-describe('time parser', () => {
-	describe('should succeed on valid time', () => {
-		test.each([
-			['00:00', 0, 0],
-			['01:05', 1, 5],
-			['03:25', 3, 25],
-			['23:59', 23, 59],
-		])('%s', (time, expectedHour, expectedMinute) => {
-			expect(TimeParser.parse(time)?.getHour()).toBe(expectedHour);
-			expect(TimeParser.parse(time)?.getMinute()).toBe(expectedMinute);
-		});
-	});
-
-	describe('should fail on invalid time format', () => {
-		test.each([['00:0'], ['0:05'], ['304:25'], ['25a'], [''], ['23h:15m'], ['23#15']])('%s', time => {
-			expect<Time | undefined>(TimeParser.parse(time)).toBe(undefined);
-		});
-	});
-
-	describe('should fail on time out of bounds', () => {
-		test.each([['-01:00'], ['24:05'], ['26:05'], ['05:60'], ['05:99']])('%s', time => {
-			expect<Time | undefined>(TimeParser.parse(time)).toBe(undefined);
-		});
-	});
-});
-
-describe('time class', () => {
-	describe('should not be possible to crete invalid time', () => {
-		let time: Time;
-
-		beforeEach(() => {
-			time = new Time();
-		});
-
-		test('set minute to 100', () => {
-			time.setMinute(100);
-			expect(time.getMinute()).not.toEqual(100);
-		});
-
-		test('set minute to -1', () => {
-			time.setMinute(-1);
-			expect(time.getMinute()).not.toEqual(-1);
-		});
-
-		test("set minute to 'a'", () => {
-			time.setMinuteFromString('a');
-			expect(time.getMinute()).not.toEqual('a');
-		});
-
-		test('set hour to 50', () => {
-			time.setHour(50);
-			expect(time.getHour()).not.toEqual(50);
-		});
-
-		test('set hour to -1', () => {
-			time.setHour(-1);
-			expect(time.getHour()).not.toEqual(-1);
-		});
-
-		test("set hour to 'a'", () => {
-			time.setHourFromString('a');
-			expect(time.getHour()).not.toEqual('a');
-		});
-	});
-});
+export {};
