d5dd0968a014595926d280db63ae81831aedfb87
Moritz Jung <m.projects.code@gmail.com>
2024-05-01 20:51:33 +0200

    maybe fix release 2

--- automation/installScript.ts
+++ automation/installScript.ts
@@ -3,14 +3,10 @@ import config from './config.json';
 
 async function installScript() {
 	for (const corePackage of config.corePackages) {
-		await $('rm -rf "node_modules"', `packages/${corePackage}`, Verboseness.VERBOSE);
-		await $('rm "bun.lockb"', `packages/${corePackage}`, Verboseness.VERBOSE);
 		await $('bun i', `packages/${corePackage}`, Verboseness.VERBOSE);
 	}
 
 	for (const nonCorePackage of config.packages) {
-		await $('rm -rf "node_modules"', `packages/${nonCorePackage}`, Verboseness.VERBOSE);
-		await $('rm "bun.lockb"', `packages/${nonCorePackage}`, Verboseness.VERBOSE);
 		await $('bun i', `packages/${nonCorePackage}`, Verboseness.NORMAL);
 	}
 }
---
+++ automation/installScriptClean.ts
@@ -0,0 +1,18 @@
+import { $, Verboseness } from 'utils/shellUtils';
+import config from './config.json';
+
+async function installScriptClean() {
+	for (const corePackage of config.corePackages) {
+		await $('rm -rf "node_modules"', `packages/${corePackage}`, Verboseness.VERBOSE);
+		await $('rm "bun.lockb"', `packages/${corePackage}`, Verboseness.VERBOSE);
+		await $('bun i', `packages/${corePackage}`, Verboseness.VERBOSE);
+	}
+
+	for (const nonCorePackage of config.packages) {
+		await $('rm -rf "node_modules"', `packages/${nonCorePackage}`, Verboseness.VERBOSE);
+		await $('rm "bun.lockb"', `packages/${nonCorePackage}`, Verboseness.VERBOSE);
+		await $('bun i', `packages/${nonCorePackage}`, Verboseness.NORMAL);
+	}
+}
+
+await installScriptClean();
