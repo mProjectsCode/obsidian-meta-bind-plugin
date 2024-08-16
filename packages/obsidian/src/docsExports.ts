c1fb65bd5e74aeb34389880a6d1177f4a59a096a
Moritz Jung <m.projects.code@gmail.com>
2024-08-16 23:44:38 +0200

    update doc exports

--- packages/obsidian/src/docsExports.ts
+++ packages/obsidian/src/docsExports.ts
@@ -1,4 +1,15 @@
 export * from 'packages/obsidian/src/ObsidianAPI';
 export * from 'packages/core/src/config/APIConfigs';
+
 export { Mountable } from 'packages/core/src/utils/Mountable';
 export { FieldMountable } from 'packages/core/src/fields/FieldMountable';
+
+export * from 'packages/core/src/parsers/FieldDeclaration';
+export * from 'packages/core/src/parsers/inputFieldParser/InputFieldDeclaration';
+export * from 'packages/core/src/parsers/bindTargetParser/BindTargetDeclaration';
+export * from 'packages/core/src/parsers/viewFieldParser/ViewFieldDeclaration';
+export type {
+	ButtonGroupDeclaration,
+	ButtonDeclaration,
+	SimpleButtonGroupDeclaration,
+} from 'packages/core/src/parsers/ButtonParser';
