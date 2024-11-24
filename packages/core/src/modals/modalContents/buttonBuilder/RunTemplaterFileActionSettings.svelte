88be266340dd6ceb8c999a41359fd0b6e545b7c2
Zain <reply2zain@gmail.com>
2024-11-23 20:04:43 -0500

    Add runTemplateFile config to button builder

---
+++ packages/core/src/modals/modalContents/buttonBuilder/RunTemplaterFileActionSettings.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { ButtonAction, RunTemplaterFileButtonAction } from 'packages/core/src/config/ButtonConfig';
+	import { ButtonActionType, ButtonStyleType } from 'packages/core/src/config/ButtonConfig';
+	import type { IPlugin } from 'packages/core/src/IPlugin';
+	import Button from 'packages/core/src/utils/components/Button.svelte';
+	import SettingComponent from 'packages/core/src/utils/components/SettingComponent.svelte';
+	import Toggle from 'packages/core/src/utils/components/Toggle.svelte';
+
+	const {
+		plugin,
+		action,
+	}: {
+		plugin: IPlugin;
+		action: RunTemplaterFileButtonAction;
+	} = $props();
+
+</script>
+
+<SettingComponent name="File path: {action.templateFile || 'default'}" description="The path from the vault to the templater file.">
+	<input type="text" bind:value={action.templateFile} placeholder="some path" />
+</SettingComponent>
+
+
